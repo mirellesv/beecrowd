7f16a145186bd93be87a8bfc7e345aefaea2c796
Mirelle Silva <mirelle.sv.vieira@gmail.com>
2023-03-08 11:29:57 -0300

    6th day

--- beginner/1018.cpp
+++ beginner/1018.cpp
@@ -3,33 +3,23 @@
 using namespace std;
 
 int main(){
-    int value, hund, fift, twt, ten, fiv, two, one, rem;
+    int i, value, quant_bkn[7], rem;
+
+    int bkn[7] = {100, 50, 20, 10, 5, 2, 1};
 
     cin >> value;
 
-    rem = 0;
+    rem = value;
 
-    hund = value/100;
-    rem = value%100;
-    fift = rem/50;
-    rem = rem%50;
-    twt = rem/20;
-    rem = rem%20;
-    ten = rem/10;
-    rem = rem%10;
-    fiv = rem/5;
-    rem = rem%5;
-    two = rem/2;
-    rem = rem%2;
-    one = rem/1;
+    for(i = 0; i < 7; i++){
+        quant_bkn[i] = rem/bkn[i];
+        rem = rem%bkn[i];
+    }
 
     cout << value << endl;
-    cout << hund << " nota(s) de R$ 100,00" << endl;
-    cout << fift << " nota(s) de R$ 50,00" << endl;
-    cout << twt << " nota(s) de R$ 20,00" << endl;
-    cout << ten << " nota(s) de R$ 10,00" << endl;
-    cout << fiv << " nota(s) de R$ 5,00" << endl;
-    cout << two << " nota(s) de R$ 2,00" << endl;
-    cout << one << " nota(s) de R$ 1,00" << endl;
+
+    for(i = 0; i < 7; i++){
+        cout << quant_bkn[i] << " nota(s) de R$ " << bkn[i] << ",00" << endl;
+    }
     return 0;
 }
