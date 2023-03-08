b0ae42ccd4625e8f4ee1a502d0adc70807b4a455
Mirelle Silva <mirelle.sv.vieira@gmail.com>
2023-03-08 10:38:13 -0300

    1018 solved

---
+++ beginner/1018.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int value, hund, fift, twt, ten, fiv, two, one, rem, i;
+
+    cin >> value;
+
+    rem = 0;
+
+    hund = value/100;
+    rem = value%100;
+    fift = rem/50;
+    rem = rem%50;
+    twt = rem/20;
+    rem = rem%20;
+    ten = rem/10;
+    rem = rem%10;
+    fiv = rem/5;
+    rem = rem%5;
+    two = rem/2;
+    rem = rem%2;
+    one = rem/1;
+
+    cout << hund << " nota(s) de R$ 100,00" << endl;
+    cout << fift << " nota(s) de R$ 50,00" << endl;
+    cout << twt << " nota(s) de R$ 20,00" << endl;
+    cout << ten << " nota(s) de R$ 10,00" << endl;
+    cout << fiv << " nota(s) de R$ 5,00" << endl;
+    cout << two << " nota(s) de R$ 2,00" << endl;
+    cout << one << " nota(s) de R$ 1,00" << endl;
+    return 0;
+}
